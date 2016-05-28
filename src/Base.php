04e5f669a8075bb04c97c95cda425b1a5034dbe5
Tomaz Lovrec <tomaz.lovrec@acl.at>
2016-05-28 20:23:00 +0200

    add basic base view class
    this class still needs to include layout loading, subview loading, etc.
    this is just a first draft

---
+++ src/Base.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Base View
+ *
+ * Base view which all View classes should extend from. The Base View handles loading
+ * of templates and adding them to the Response object.
+ *
+ * @package   SlaxWeb\View
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.3
+ */
+namespace SlaxWeb\View;
+
+use SlaxWeb\Config\Container as Config;
+use SlaxWeb\View\AbstractLoader as Loader;
+
+class Base
+{
+    /**
+     * Template name
+     *
+     * @var string
+     */
+    public $template = "";
+
+    /**
+     * Config
+     *
+     * @var \SlaxWeb\Config\Container
+     */
+    protected $_config = null;
+
+    /**
+     * Template Loader
+     *
+     * @var \SlaxWeb\View\AbstractLoader
+     */
+    protected $_loader = null;
+
+    /**
+     * Class constructor
+     *
+     * Instantiate the view, by assigning its dependencies to the class properties.
+     * Set the base directory for the template files, and set the template name
+     * if none is already set by an override property and config permits it.
+     *
+     * @param \SlaxWeb\Config\Container $config Configuration container
+     * @param \SlaxWeb\View\AbstractLoader $loader Template file loader
+     * @return void
+     */
+    public function __construct(Config $config, Loader $loader)
+    {
+        $this->_config = $config;
+        $this->_loader = $loader;
+
+        $this->_loader->setTemplateDir($config["template.baseDir"]);
+
+        if ($this->template === "" && $config["template.autoTplName"] === true) {
+            $this->template = substr(get_class($this), strrpos("\\") + 1);
+        }
+    }
+
+    /**
+     * Render view
+     *
+     * Renders the view by rendering the template with the provided template loader.
+     *
+     * @param array $data Template data to be passed to the template. Default []
+     * @param int $return Output or return rendered template. Default self::TPL_OUTPUT
+     * @param int $cacheData Cache template data. Default self::TPL_CACHE_VARS
+     * @return mixed
+     */
+    public function render(
+        array $data = [],
+        int $return = Loader::TPL_OUTPUT,
+        int $cacheData = Loader::TPL_CACHE_VARS
+    ) {
+        $this->_loader->setTemplate($this->template);
+        try {
+            $buffer = $this->_loader->render($data, $return, $cacheData);
+        } catch (Exception\TemplateNotFoundException $e) {
+            // @todo: display error message
+            return false;
+        }
+        if ($return === Loader::TPL_RETURN) {
+            return $buffer;
+        }
+
+        return true;
+    }
+}
